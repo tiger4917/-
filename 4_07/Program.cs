49852f56e5739eed9f824844f37d602bdb48e4ab
tiger4917 <tiger4917@mail.ru>
2022-07-04 10:07:11 +0300

    задача на возведение в степень

---
+++ 4_07/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("Введите число: ");
+double num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите значение степени: ");
+double n = Convert.ToInt32(Console.ReadLine());
+double result = Math.Pow(num ,n);
+System.Console.WriteLine($"Число {num}-> {result}");
